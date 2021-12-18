a56e31e09807b4deb8693dd11ba949be016fab7c
Danielle Bencze <daniellebencze@live.com>
2021-12-18 12:09:28 -0800

    commit tree 0dd4a8d5d6a90fa62d94361b2a0c89e615c29f03

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// Code your solution in this file!
-const companyName = 'Scuber';
-let mostProfitableNeighborhood = 'Chelsea';
-let companyCeo = 'Susan Smith';
+// // Code your solution in this file!
+// const companyName = 'Scuber';
+// let mostProfitableNeighborhood = 'Chelsea';
+// let companyCeo = 'Susan Smith';
+
+const companyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
